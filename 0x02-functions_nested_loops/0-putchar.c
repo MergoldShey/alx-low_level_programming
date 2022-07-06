cd7229fa2ef86a527e609573c571c77a4d0ba011
root <Mergoldshey>
2022-07-06 01:32:48 -0700

    A program that prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
 */
 int main(void)
 {
-char test[9] = "_putchar";
-int i;
-i = 0;
-while (i < 8)
-{
-_putchar (test[i]);
-i++;
-}
-putchar("\n");
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar("\n");
 return (0);
 }
