d519d419ac926101aefb3cbd544723cf63d8643e
root <Mergoldshey>
2022-07-06 07:57:06 -0700

    A program that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 /**
-* main - computes and prints the sum of all the multiples
-* of 3 or 5 below 1024
+* main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+* followed by a new line
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-unsigned long int sum3, sum5, sum0;
-int i;
-sum3 = 0;
-sum5 = 0;
-sum = 0;
-for (i = 0; i < 1024; ++i)
+long int i, j, k, next;
+j = 1;
+k = 2;
+for (i = 1; i <= 50; ++i)
 {
-if ((i % 3) == 0)
-}
-sum3 = sum3 + i;
-}
-else if ((i % 5) == 0)
+if (j != 20365011074)
 {
-sum5 = sum5 + i;
+printf("%ld, ", j);
 }
+else
+{
+printf("%ld\n", j);
 }
-sum = sum3 + sum5;
-printf("%lu\n", sum);
+next = j + k;
+j = k;
+k = next;
+}
 return (0);
 }
