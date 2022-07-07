860eaa05e9569b0db8debbdb19c47cf50d224e6b
root <Mergoldshey>
2022-07-07 08:04:28 -0700

    A test function that prints if integer is positive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,26 +1,12 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 /**
-* main - Determine if a random number is positive, negative or zero.
-* Return: 0 on success
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n < 0)
-{
-printf("%d is %s\n", n, "negative");
-}
-else if (n > 0)
-{
-printf("%d is %s\n", n, "positive");
-}
-else
-{
-printf("%d is %s\n", n, "zero");
-}
+int i;
+i = 98;
+positive_or_negative(i);
 return (0);
 }
