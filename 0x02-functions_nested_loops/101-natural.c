4478f98dc694b163c6815e4f7c207f391bc939a9
root <Mergoldshey>
2022-07-06 19:51:05 -0700

    A program that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,7 +2,7 @@
 /**
 * main - computes and prints the sum of all the multiples
 * of 3 or 5 below 1024
-* Return: Always 0 (Success)
+* Return: Always 0.
 */
 int main(void)
 {
@@ -10,7 +10,7 @@ unsigned long int sum3, sum5, sum0;
 int i;
 sum3 = 0;
 sum5 = 0;
-sum = 0;
+sum0 = 0;
 for (i = 0; i < 1024; ++i)
 {
 if ((i % 3) == 0)
