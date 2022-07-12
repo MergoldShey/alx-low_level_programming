1a420b54f490b332d936583208e6b6c25647eeb0
mergoldshey@gmail.com <mergoldshey@gmail.com>
2022-07-12 22:51:09 +0000

    A function that prints a string, followed by a new line, to stdout

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_rev - Print Reversed string
+* @s: The strng to be reversed
+*
+* Return: Nothing
+*/
+
+void print_rev(char *s)
+{
+int counter = 0;
+while (s[counter])
+counter++;
+while (counter--)
+{
+_putchar(s[counter]);
+}
+_putchar('\n');
+}
