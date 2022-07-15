ba3361fbdeecca6710d776b9476c235a72903994
mergoldshey@gmail.com <mergoldshey@gmail.com>
2022-07-15 00:20:14 +0000

    A function that compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,20 +12,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int i, status;
+int i;
 
-i = 0;
-while (s1[i] != '\0' || s2[i] != '\0')
+for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 {
 if (s1[i] != s2[i])
-{
-status = s1[i] - s2[i];
-break;
+return (s1[i] - s2[i]);
 }
-else
-{
 return (0);
-}
-}
-return (status);
 }
