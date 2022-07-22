ec90c0b794f58f2401fae9cc6767ae523e828a49
root <Mergoldshey>
2022-07-22 15:41:32 -0700

    A program that prints the number of arguments passed into it

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,44 +1,29 @@
 #include <stdio.h>
-#include<stdlib.h>
+
+#include <stdlib.h>
 #include <ctype.h>
-
 /**
- * main - This Program prints the name of the program
- * @argc: number of arguements
- * @argv: array of arguements
- *
- * Return: An integer
+ * main - adds positive numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-
 int main(int argc, char *argv[])
 {
-	int add = 0,  num, k, j, i;
+int i, j, add = 0;
 
-	if (argc < 2)
-	{
-		printf("%d\n", 0);
-
-	}
-	else
-	{
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > '9' || argv[i][j] < '0')
-			{
-				puts("error");
-				return (1);
-			}
-		}
-	}
-	for (k = 1; k < argc; k++)
-	{
-		num = atoi(argv[k]);
-		add += num;
-	}
-	printf("%d\n", add);
-
-	}
-	return (0);
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
+{
+printf("Error\n");
+return (1);
+}
+}
+add += atoi(argv[i]);
+}
+printf("%d\n", add);
+return (0);
 }
